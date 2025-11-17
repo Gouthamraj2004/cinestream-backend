3c45ab10732eb524db077de5e302f71db6edab65
Gouthamraj R <83597052+Gouthamraj2004@users.noreply.github.com>
2025-11-17 23:07:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const watchlistRoutes = require('./routes/watchlist');
+const movieRoutes = require('./routes/movie');
 const authRoutes = require('./routes/auth'); 
-
 
 dotenv.config();
 
@@ -12,21 +12,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-
-app.use('/api/watchlist', watchlistRoutes);
-
-// Connect to MongoDB (replace <DB_CONN_STRING>)
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/cinestream', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch((err) => console.error('MongoDB error:', err));
-
 // Routes
-const movieRoutes = require('./routes/movie');
+app.use('/api/auth', authRoutes);
+app.use('/api/watchlist', watchlistRoutes);
 app.use('/api/movies', movieRoutes);
+
+// Updated MongoDB Atlas URI using your credentials
+const MONGO_URI =
+  process.env.MONGO_URI ||
+  'mongodb+srv://gouthamraj318:Gouthamraj@2904@cluster0.xsw3bk9.mongodb.net/cinestream?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB error:', err));
 
 // Start server
 const PORT = process.env.PORT || 5000;
